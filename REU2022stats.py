c0f45772406bd69dfdd177371a8669c6d815ee70
Salvador Guel <Salvadorguel8@gmail.com>
2022-07-14 02:00:58 -0500

    Added REU2022stats.py.

---
+++ REU2022stats.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 14 01:53:36 2022
+
+@author: salvadorguel
+"""
+
+#def average():
+    #Need to check how to pass in an array as an arguement
+    
